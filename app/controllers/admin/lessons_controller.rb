6caaf48069cc174ba2da1a58b71bfd83056259e3
Alex <>
2022-05-01 11:26:09 +0300

    add lessons views and controller actions

--- app/controllers/admin/lessons_controller.rb
+++ app/controllers/admin/lessons_controller.rb
@@ -1,9 +1,55 @@
 class Admin::LessonsController < ApplicationController
+  before_action :set_lesson, only: %i[show edit update destroy]
+
   def index
-    @lessons = Lesson.all
+    @lessons = Lesson.all.order(:section_id, :order)
   end
 
-  def show
+  def show; end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def edit; end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+
+    respond_to do |format|
+      if @lesson.save
+        format.html { redirect_to(admin_lesson_path(@lesson), notice: 'Lesson was successfully created.') }
+      else
+        format.html { render(:new, status: :unprocessable_entity) }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @lesson.update(lesson_params)
+        format.html { redirect_to(admin_lesson_path(@lesson), notice: 'Lesson was successfully updated.') }
+      else
+        format.html { render(:edit, status: :unprocessable_entity) }
+      end
+    end
+  end
+
+  def destroy
+    @lesson.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(admin_lesson_path, notice: 'Lesson was successfully destroyed.') }
+    end
+  end
+
+  private
+
+  def set_lesson
     @lesson = Lesson.find(params[:id])
+  end
+
+  def lesson_params
+    params.require('lesson').permit(:name, :description, :order, :section_id)
   end
 end
