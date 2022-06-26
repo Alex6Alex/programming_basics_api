dd0a20f7b4dfd247573229a02d1fd51d0a1cef38
Alex <>
2022-06-26 15:44:55 +0300

    fix

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::BaseController < ApplicationController
     return if request.headers['Authorization'].blank?
 
     authenticate_or_request_with_http_token do |token|
-      jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+      jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base || ENV['SECRET_KEY_BASE']).first
 
       @current_user_id = jwt_payload['id']
       puts @current_user_id
