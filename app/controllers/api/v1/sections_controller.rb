a901c487c9f7e2615da05b6d3be45fff1ccd7ec5
Alex <>
2022-06-26 17:31:08 +0300

    fix

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::SectionsController < Api::V1::BaseController
   def index
-    redirect_to('https://google.com/', allow_other_host: true)
-    # @sections = Section.preload(:lessons).all
+    @sections = Section.preload(:lessons).all
   end
 end
